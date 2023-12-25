68eab97a2f13bdcc6e9ced869bae726eaabde484
Amine Aouragh <amine.aouragh.20@gmail.com>
2023-12-25 20:07:52 +0100

    add vercel analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import RecommendationPage from "./RecommendationPage"
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   return (
-    <RecommendationPage />
+    <>
+     <RecommendationPage />
+     <Analytics />
+    </>
   )
 }
